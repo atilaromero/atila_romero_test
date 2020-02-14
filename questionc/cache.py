85a51aaa3ac50f0b08aa391286af9579ab068b9b
Atila Romero <atilaromero@gmail.com>
2020-02-13 21:50:45 -0300

    tests passing

--- questionc/cache.py
+++ questionc/cache.py
@@ -17,6 +17,7 @@ class LRUCache:
         self._timeout = secondstimeout
         self._cache : Dict[CacheData] = {}
         self._head = None
+        self._tail = None
 
     def get(self, key):
         if not key in self._cache:
@@ -45,6 +46,7 @@ class LRUCache:
     def _promote(self, cacheData : CacheData):
         """Move cacheData to the top"""
         if self._head is cacheData:
+            cacheData.stamp = time.time()
             return
         if cacheData.prev != None:
             prev = cacheData.prev
@@ -52,6 +54,10 @@ class LRUCache:
         if cacheData.next != None:
             next_ : CacheData = cacheData.next
             next_.prev = cacheData.prev
+        if self._tail == cacheData:
+            self._tail = cacheData.prev
+        if self._tail is None:
+            self._tail = cacheData
         cacheData.stamp = time.time()
         cacheData.prev = None
         cacheData.next = self._head
@@ -60,14 +66,24 @@ class LRUCache:
         self._head = cacheData
 
     def _evict(self, key):
-        """Remove cache[key] and all its descendants from cache"""
+        """Remove cache[key] and all its descendants from cache.
+            Deleting the circular references makes things easier for the GIL
+        """
         cacheData : CacheData = self._cache[key]
         while not cacheData is None:
             del self._cache[cacheData.key]
+            if not cacheData.prev is None:
+                cacheData.prev.next = None
             cacheData.prev = None
             next_ = cacheData.next
             cacheData.next = None
+            if self._head == cacheData:
+                self._head = None
+            if self._tail == cacheData:
+                self._tail = None
             cacheData = next_
+
+
 
 def resolve(domain):
     return "1.2.3.4"
--- questionc/test_cache.py
+++ questionc/test_cache.py
@@ -1,6 +1,5 @@
 import unittest
-from cache import LRUCache
-
+from cache import LRUCache, CacheData
 
 class QuestionC(unittest.TestCase):
     def test_get1(self):
@@ -54,9 +53,77 @@ class QuestionC(unittest.TestCase):
         self.assertEqual(cache._head.key, 99)
         self.assertEqual(cache._head.prev, None)
         self.assertEqual(cache._cache[0].next, None)
+        self.assertEqual(cache._head.key, 99)
         cache._evict(49) # evict from 49 to 0, 50-99 are newer 
         self.assertEqual(count, 100)
+        self.assertEqual(cache._head.key, 99)
+        self.assertEqual(cache._tail.key, 50)
         self.assertEqual(len(cache._cache), 50)
+        self.assertEqual(cache._cache[50].prev.key, 51)
+        self.assertEqual(cache._cache[50].next, None)
+        cache._evict(cache._head.key)
+        self.assertEqual(len(cache._cache), 0)
+        self.assertEqual(cache._head, None)
+        self.assertEqual(cache._tail, None)
 
+    def test_promote(self):
+        count = 0
+        def dummyLoadCB(key):
+            nonlocal count
+            count += 1
+            return {"mydata": key}
+        cache = LRUCache(dummyLoadCB, 0.5)
+        for x in range(10):
+            cache.get(x)
+        cache._promote(cache._cache[5])
+        self.assertEqual(cache._head.key, 5)
+        self.assertEqual(cache._cache[5].prev, None)
+        self.assertEqual(cache._cache[5].next.key, 9)
+        self.assertEqual(cache._cache[9].prev.key, 5)
+        self.assertEqual(cache._cache[9].next.key, 8)
+        self.assertEqual(cache._cache[8].prev.key, 9)
+        self.assertEqual(cache._cache[8].next.key, 7)
+        self.assertEqual(cache._cache[7].prev.key, 8)
+        self.assertEqual(cache._cache[7].next.key, 6)
+        self.assertEqual(cache._cache[6].prev.key, 7)
+        self.assertEqual(cache._cache[6].next.key, 4)
+        self.assertEqual(cache._cache[4].prev.key, 6)
+        self.assertEqual(cache._cache[4].next.key, 3)
+        self.assertEqual(cache._cache[0].prev.key, 1)
+        self.assertEqual(cache._cache[0].next, None)
+        cache._promote(cache._cache[0])
+        self.assertEqual(cache._cache[0].prev, None)
+        self.assertEqual(cache._cache[0].next.key, 5)
+        self.assertEqual(cache._cache[1].prev.key, 2)
+        self.assertEqual(cache._cache[1].next, None)
+        cache._promote(cache._cache[0])
+        self.assertEqual(cache._cache[0].prev, None)
+        self.assertEqual(cache._cache[0].next.key, 5)
+        self.assertEqual(cache._cache[5].prev.key, 0)
+        self.assertEqual(cache._cache[5].next.key, 9)
+        cache._promote(cache._cache[5])
+        self.assertEqual(cache._cache[5].prev, None)
+        self.assertEqual(cache._cache[5].next.key, 0)
+        self.assertEqual(cache._cache[0].prev.key, 5)
+        self.assertEqual(cache._cache[0].next.key, 9)
+        self.assertEqual(cache._cache[9].prev.key, 0)
+        self.assertEqual(cache._cache[9].next.key, 8)
 
-
+    def test_store_promote(self):
+        count = 0
+        def dummyLoadCB(key):
+            nonlocal count
+            count += 1
+            return {"mydata": key}
+        cache = LRUCache(dummyLoadCB, 0.5)
+        cache._store(10, 10)
+        self.assertEqual(cache._head.key, 10)
+        self.assertEqual(cache._cache[10].prev, None)
+        self.assertEqual(cache._cache[10].next, None)
+        self.assertNotEqual(cache._cache[10].stamp, 0)
+        prevStamp = cache._cache[10].stamp
+        cache._promote(cache._cache[10])
+        self.assertEqual(cache._head.key, 10)
+        self.assertEqual(cache._cache[10].prev, None)
+        self.assertEqual(cache._cache[10].next, None)
+        self.assertGreater(cache._cache[10].stamp, prevStamp)
